d419fc1136385f3c86149c8cb3c07e194cd7984c
Dung Dang <58243954+chachajona@users.noreply.github.com>
2023-10-06 15:52:36 +0700

    Modify theme reload

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,10 +3,11 @@ import React, { useState, createContext } from "react";
 export const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("#BD2126");
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "#BD2126");
 
   const changeTheme = (newTheme) => {
     setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
   }
   return (
     <ThemeContext.Provider
